72f6a9e6c3fab42abe100a15b3e748facbe87a5d
Egelke BVBA <info@egelke.net>
2021-12-23 13:35:49 +0100

    Added some stuff to eID test, tested it against the EU DSS service.

--- XadesTest/EIDTest.cs
+++ XadesTest/EIDTest.cs
@@ -1,4 +1,5 @@
-﻿using Egelke.Eid.Client;
+﻿using Egelke.EHealth.Client.Pki;
+using Egelke.Eid.Client;
 using IM.Xades.Extra;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,7 @@ namespace IM.Xades.Test
     public class EIDTest
     {
         [Fact]
-        public void CurrentEID()
+        public void XadesBes()
         {
             X509Certificate2 sign;
             using (var readers = new Readers(ReaderScope.User))
@@ -26,29 +27,76 @@ namespace IM.Xades.Test
                 target.Open();
 
                 store.Open(OpenFlags.ReadOnly);
-                sign = store.Certificates.Find(X509FindType.FindByThumbprint, target.AuthCert.Thumbprint, false)[0];
+                sign = store.Certificates.Find(X509FindType.FindByThumbprint, target.SignCert.Thumbprint, false)[0];
             }
 
             var document = new XmlDocument();
             document.PreserveWhitespace = true;
             document.Load(@"data\basic.xml");
-            var xsigner = new XadesCreator(sign);
+            var xsigner = new XadesCreator(sign, true);
             var xades = xsigner.CreateXadesBes(document, "_1");
+            document.DocumentElement.AppendChild(xades);
 
             // Output for reading
             MemoryStream stream = new MemoryStream();
             using (var writer = XmlWriter.Create(stream))
             {
-                xades.WriteTo(writer);
+                document.WriteTo(writer);
             }
             stream.Seek(0, SeekOrigin.Begin);
 
-            var xades2 = new XmlDocument();
-            xades2.PreserveWhitespace = true;
-            xades2.Load(stream);
+            var signed = new XmlDocument();
+            signed.PreserveWhitespace = true;
+            signed.Load(stream);
 
             var xerifier = new XadesVerifier();
-            var info = xerifier.Verify(document, (XmlElement)XadesTools.FindXadesProperties(xades2)[0]);
+            var info = xerifier.Verify(signed, (XmlElement)XadesTools.FindXadesProperties(signed)[0]);
+
+            Assert.NotNull(info);
+            Assert.NotNull(info.Certificate);
+            Assert.Equal(sign, info.Certificate);
+            Assert.Equal(XadesForm.XadesBes, info.Form);
+            Assert.NotNull(info.Time);
+            Assert.True((DateTimeOffset.Now - info.Time.Value) < new TimeSpan(0, 5, 0));
+        }
+
+        [Fact]
+        public void XadesT()
+        {
+            X509Certificate2 sign;
+            using (var readers = new Readers(ReaderScope.User))
+            using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                var target = (EidCard)readers.ListCards().Where(c => c is EidCard).FirstOrDefault();
+                Assert.True(target != null, "No eid inserted, please insert (test) eid");
+                target.Open();
+
+                store.Open(OpenFlags.ReadOnly);
+                sign = store.Certificates.Find(X509FindType.FindByThumbprint, target.SignCert.Thumbprint, false)[0];
+            }
+
+            var document = new XmlDocument();
+            document.PreserveWhitespace = true;
+            document.Load(@"data\basic.xml");
+            var xsigner = new XadesCreator(sign, true);
+            xsigner.TimestampProvider = new Rfc3161TimestampProvider();
+            var xades = xsigner.CreateXadesT(document, "_1");
+            document.DocumentElement.AppendChild(xades);
+
+            // Output for reading
+            MemoryStream stream = new MemoryStream();
+            using (var writer = XmlWriter.Create(stream))
+            {
+                document.WriteTo(writer);
+            }
+            stream.Seek(0, SeekOrigin.Begin);
+
+            var signed = new XmlDocument();
+            signed.PreserveWhitespace = true;
+            signed.Load(stream);
+
+            var xerifier = new XadesVerifier();
+            var info = xerifier.Verify(signed, (XmlElement)XadesTools.FindXadesProperties(signed)[0]);
 
             Assert.NotNull(info);
             Assert.NotNull(info.Certificate);
